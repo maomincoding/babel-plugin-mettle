87b9f92ed15ef06555eb3fefb185d66a7fa0c36b
maomincoding <maomincoding@163.com>
2025-11-05 23:56:37 +0800

    release: v0.8.0

--- src/index.js
+++ src/index.js
@@ -3,28 +3,6 @@ module.exports = function ({ types: t }) {
     return /^[A-Z]/.test(str);
   }
 
-  function isSignalBinding(binding) {
-    if (!binding) return false;
-    const { init } = binding.path.node;
-    return (
-      t.isCallExpression(init) &&
-      t.isIdentifier(init.callee) &&
-      (init.callee.name === 'signal' || init.callee.name === 'computed')
-    );
-  }
-
-  function isInsideCustomComponentAttribute(path) {
-    const parent = path.parentPath;
-    if (t.isJSXAttribute(parent.node)) {
-      const element = parent.parentPath?.parentPath?.node.openingElement;
-      if (t.isJSXOpeningElement(element) && t.isJSXIdentifier(element.name)) {
-        const tagName = element.name.name;
-        return isFirstCharUpperCase(tagName);
-      }
-    }
-    return false;
-  }
-
   return {
     name: 'babel-plugin-mettle',
     visitor: {
@@ -40,55 +18,6 @@ module.exports = function ({ types: t }) {
             );
 
             returnStatement.argument = newFunction;
-          }
-        }
-      },
-      JSXExpressionContainer(path) {
-        const { expression } = path.node;
-
-        if (t.isIdentifier(expression)) {
-          const name = expression.name;
-          const binding = path.scope.getBinding(name);
-          if ((binding && isSignalBinding(binding)) || name.endsWith('$')) {
-            if (!isInsideCustomComponentAttribute(path)) {
-              path.node.expression = t.memberExpression(t.identifier(name), t.identifier('value'));
-            }
-          }
-        }
-
-        // Object value
-        if (t.isMemberExpression(expression)) {
-          if (t.isIdentifier(expression.object)) {
-            const name = expression.object.name;
-            const binding = path.scope.getBinding(name);
-            if ((binding && isSignalBinding(binding)) || name.endsWith('$')) {
-              expression.object = t.memberExpression(expression.object, t.identifier('value'));
-            }
-          }
-        }
-
-        // Member Expression
-        if (t.isCallExpression(expression)) {
-          if (t.isMemberExpression(expression.callee) && t.isIdentifier(expression.callee.object)) {
-            const name = expression.callee.object.name;
-            const binding = path.scope.getBinding(name);
-            if ((binding && isSignalBinding(binding)) || name.endsWith('$')) {
-              expression.callee.object = t.memberExpression(
-                expression.callee.object,
-                t.identifier('value')
-              );
-            }
-          }
-        }
-
-        // Conditional Expressions
-        if (t.isConditionalExpression(expression)) {
-          if (t.isIdentifier(expression.test)) {
-            const name = expression.test.name;
-            const binding = path.scope.getBinding(name);
-            if ((binding && isSignalBinding(binding)) || name.endsWith('$')) {
-              expression.test = t.memberExpression(expression.test, t.identifier('value'));
-            }
           }
         }
       },
