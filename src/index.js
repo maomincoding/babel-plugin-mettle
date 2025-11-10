06f611b092ab7ed8dae464df14a8b948290ae79d
maomincoding <maomincoding@163.com>
2025-11-10 23:04:08 +0800

    release: v0.8.5

--- src/index.js
+++ src/index.js
@@ -1,25 +1,78 @@
 module.exports = function ({ types: t }) {
   function isFirstCharUpperCase(str) {
-    return /^[A-Z]/.test(str);
+    return str && /^[A-Z]/.test(str);
+  }
+
+  function getFunctionName(path) {
+    if (path.node.id) {
+      return path.node.id.name;
+    }
+
+    if (t.isVariableDeclarator(path.parent)) {
+      return path.parent.id.name;
+    }
+
+    if (t.isAssignmentExpression(path.parent)) {
+      return path.parent.left.property?.name;
+    }
+
+    if (t.isProperty(path.parent)) {
+      return path.parent.key.name;
+    }
+
+    return null;
+  }
+
+  function processFunction(path) {
+    const functionName = getFunctionName(path);
+
+    if (!isFirstCharUpperCase(functionName)) {
+      return;
+    }
+
+    path.traverse({
+      ReturnStatement(returnPath) {
+        if (returnPath.getFunctionParent() === path && returnPath.node.argument) {
+          const newFunction = t.functionExpression(
+            null,
+            [],
+            t.blockStatement([t.returnStatement(returnPath.node.argument)])
+          );
+          returnPath.node.argument = newFunction;
+        }
+      },
+
+      FunctionDeclaration(innerPath) {
+        if (innerPath !== path) {
+          processFunction(innerPath);
+          innerPath.skip();
+        }
+      },
+
+      FunctionExpression(innerPath) {
+        if (innerPath !== path) {
+          processFunction(innerPath);
+          innerPath.skip();
+        }
+      },
+
+      ArrowFunctionExpression(innerPath) {
+        if (innerPath !== path) {
+          processFunction(innerPath);
+          innerPath.skip();
+        }
+      },
+    });
   }
 
   return {
     name: 'babel-plugin-mettle',
     visitor: {
       FunctionDeclaration(path) {
-        if (path.node.id && isFirstCharUpperCase(path.node.id.name)) {
-          const returnStatement = path.node.body.body.find((node) => t.isReturnStatement(node));
-
-          if (returnStatement && returnStatement.argument) {
-            const newFunction = t.functionExpression(
-              null,
-              [],
-              t.blockStatement([t.returnStatement(returnStatement.argument)])
-            );
-
-            returnStatement.argument = newFunction;
-          }
-        }
+        processFunction(path);
+      },
+      FunctionExpression(path) {
+        processFunction(path);
       },
     },
   };
